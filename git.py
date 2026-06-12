174b55ef1ecf06022994e7488c8e1ef0b6f74a1c
kanimozhi <kanimozhi21792@gmail.com>
2026-06-12 16:57:10 +0530

    git.py

--- git.py
+++ git.py
@@ -1,5 +1,25 @@
-name = input("Enter your name: ")
-city = input("Enter your city: ")
+# age = int(input("enter your age:"))
+# has_license = input("do you have license (y/n):")
 
-print("Hello", name)
-print("Welcome to", city)
+# if(age>18 and has_license == "y"):
+#     print("you can drive")
+# elif(age>18 and has_license =="n"):
+#     print("you cannot drive")
+# else:
+#     (age<18)
+#     print("you are under age you cannot drive")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Addition =", num1 + num2)
+print("Subtraction =", num1 - num2)
+print("Multiplication =", num1 * num2)
+print("Division =", num1 / num2)
+
+
+
+
+
+
+
+
