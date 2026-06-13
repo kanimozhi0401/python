c18cd3b6c23fbdb814fc217786e6d34e4240d7c2
kanimozhi <kanimozhi21792@gmail.com>
2026-06-13 14:35:09 +0530

    if statement

--- task3.py
+++ task3.py
@@ -1,13 +1,4 @@
-# age = int(input("enter your age:"))
-# has_license = input("do you have license (y/n):")
 
-# if(age>18 and has_license == "y"):
-#     print("you can drive")
-# elif(age>18 and has_license =="n"):
-#     print("you cannot drive")
-# else:
-#     (age<18)
-#     print("you are under age you cannot drive")
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
